4b22e740f7ac679a56b68245dff0ac35309fba2f
JKBoy2244 <jobayer2006@gmail.com>
2026-09-16 19:37:11 +0100

    Update metrics.cpp

--- src/metrics.cpp
+++ src/metrics.cpp
@@ -10,4 +10,25 @@ namespace metrics  {
 
         return static_cast<double>(totalDropped) / totalGenerated * 100.0;
     }
+
+    double calculateCongestionPercentage(int congestedTicks, int simulationDurationNumber)  {
+        
+      if (simulationDurationNumber == 0)  {
+          
+        return 0.0;
+     }
+
+    return static_cast<double>(congestedTicks) / simulationDurationNumber* 100.0;
+   }
+
+   double calculateAverageOccupancyPercentage(double averageQueue, int bufferCapacityNumber)  {
+
+       
+    if (bufferCapacityNumber == 0)  {
+        
+        return 0.0;
+    }
+
+    return averageQueue / bufferCapacityNumber* 100.0;
+  }
 }
