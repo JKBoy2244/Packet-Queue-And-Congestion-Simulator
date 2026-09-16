3f1e2cc69e7a94b2bbbcb4ef3f72f582d1c6967d
JKBoy2244 <jobayer2006@gmail.com>
2026-09-16 19:16:20 +0100

    Create test_simulation.cpp

---
+++ tests/test_simulation.cpp
@@ -0,0 +1,38 @@
+#include <cassert>
+#include <iostream>
+using namespace std;
+
+#include "../src/simulation.h"
+
+int main()  {
+  
+    for (int arrivals { 0 }; arrivals <= 1000000; ++arrivals) {
+        for (int freeSpace { 0 }; freeSpace <= 1000000; ++freeSpace) {
+          
+            int acceptedPackets {};
+
+            if (arrivals <= freeSpace)  {
+              
+                acceptedPackets = arrivals;
+            } else  {
+                acceptedPackets = freeSpace;
+            }
+
+            assert(acceptedPackets <= arrivals);
+            assert(acceptedPackets <= freeSpace);
+
+            if (arrivals <= freeSpace)
+            {
+                assert(acceptedPackets == arrivals);
+            }
+            else
+            {
+                assert(acceptedPackets == freeSpace);
+            }
+        }
+    }
+
+    std::cout << "All simulation tests successfully passed!\n";
+
+    return 0;
+}
