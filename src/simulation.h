1456594fc0278688e80dbd9c3d7043c6257c2e3e
JKBoy2244 <jobayer2006@gmail.com>
2026-09-16 17:12:57 +0100

    Add simulation header file with function declaration

---
+++ src/simulation.h
@@ -0,0 +1,9 @@
+#ifndef SIMULATION
+#define SIMULATION 
+
+namespace simulation {
+
+  void simulation(int arrivalCapacityNumber, int bufferCapacityNumber, int congestionThresholdNumber, int serviceCapacityNumber, int simulationDurationNumber, char option);
+}
+
+#endif
