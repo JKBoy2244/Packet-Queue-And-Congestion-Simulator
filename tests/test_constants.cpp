cf8590c5d36c3337ce36195fdd2bbd56527553c2
JKBoy2244 <jobayer2006@gmail.com>
2026-09-16 16:43:06 +0100

    Update test_constants.cpp

--- tests/test_constants.cpp
+++ tests/test_constants.cpp
@@ -15,6 +15,10 @@ static_assert(constants::minimumArrivalRate >= 1);
 static_assert(constants::maximumArrivalRate <= 50000);
 static_assert(constants:: minimumArrivalRate < constants::maximumArrivalRate);                     //This block enforces a strict limit of arrival rates in terms of valid ranges
 
+static_assert(constants::minimumSimulationDuration >= 1);
+static_assert(constants::maximumSimulationDuration <= 1000000);
+static_assert(constants::minimumSimulationDuration < constants::maximumSimulationDuration);         //This block enforces a strict limit of simulation duration steps
+
 static_assert(constants::minimumCongestionThresholdPercentage >= 1);
 static_assert(constants::maximumCongestionThresholdPercentage <= 100);
 static_assert(constants:: minimumCongestionThresholdPercentage < constants::maximumCongestionThresholdPercentage);    /*This block enforces a strict limit of congestion threshold proportion in terms of 
@@ -40,7 +44,13 @@ int main() {
     assert(c <= constants::maximumArrivalRate);  
   }
 
-  for (int d {1}; d <= 100; ++d) {
+  for (int d {1}; d <= 1000000; ++d) {
+
+    assert(d >= constants::minimumSimulationDuration);
+    assert(d <= constants::maximumSimulationDuration);  
+  }
+
+  for (int e {1}; e <= 100; ++d) {
 
     assert(d >= constants::minimumCongestionThresholdPercentage);
     assert(d <= constants::maximumCongestionThresholdPercentage);  
