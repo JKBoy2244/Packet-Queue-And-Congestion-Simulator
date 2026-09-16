312492dcb0866c4ffc987e7db45b4d77ce579b29
JKBoy2244 <jobayer2006@gmail.com>
2026-09-16 21:42:54 +0100

    Refactor main.cpp to include simulation headers
    Added multiple header files for input and simulation handling.

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,31 @@
 #include <iostream>
+#include <random>
+
+#include "constants.h"
+#include "bufferCapacityInput.h"
+#include "serviceCapacityInput.h"
+#include "arrivalCapacityInput.h"
+#include "simulationDurationInput.h"
+#include "congestionThresholdInput.h"
+#include "simulation.h"
+#include "metrics.h"
+#include "display.h"
+#include "detailedOutputChoose.h"
+
+using packets = int; 
+using select = char;
 
 int main() {
 
-
+  while (true) {
+    
+     packets bufferCapacityNumber = bufferCapacity::bufferCapacityInput();
+     packets serviceCapacityNumber = serviceCapacity::serviceCapacityInput();
+     packets arrivalCapacityNumber = arrivalCapacity::arrivalCapacityInput();
+     packets simulationDurationNumber = simulationDuration::simulationDurationInput();
+     packets congestionThresholdNumber = congestionThreshold::congestionThresholdInput();
+     select option = detailedOutput::detailedOutputChoose();
+     simulation::simulation(bufferCapacityNumber, serviceCapacityNumber, arrivalCapacityNumber, simulationDurationNumber, congestionThresholdNumber, option);
+  }
   return 0;
 }
