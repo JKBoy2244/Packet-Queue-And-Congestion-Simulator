0d9ea56eaeb657dbe486382fda026a0a6151e232
JKBoy2244 <jobayer2006@gmail.com>
2026-09-16 19:37:48 +0100

    Update metrics.h

--- src/metrics.h
+++ src/metrics.h
@@ -2,7 +2,10 @@
 #define METRICS
 
 namespace metrics  {
+
     double calculateDropPercentage(long long totalDropped, long long totalGenerated);
+    double calculateCongestionPercentage(int congestedTicks, int simulationDurationNumber);
+    double calculateAverageOccupancyPercentage(double averageQueue, int bufferCapacityNumber);
 }
 
 #endif
