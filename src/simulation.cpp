fccb51714a509c5e44648a46c536504dd7c90c0c
JKBoy2244 <jobayer2006@gmail.com>
2026-09-16 17:12:54 +0100

    Create simulation.cpp

---
+++ src/simulation.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <random>
+
+#include "simulation.h"
+
+namespace simulation {
+
+  void simulation(int arrivalCapacityNumber, int bufferCapacityNumber, int congestionThresholdNumber, int serviceCapacityNumber, int simulationDurationNumber, char option) {
+
+     
+    return;
+  }
+}
